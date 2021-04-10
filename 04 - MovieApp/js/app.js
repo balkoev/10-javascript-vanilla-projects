d06c183a116958c6f69a82a4e21da7066172d5c3
Adam Balkoev <adam22185515@gmail.com>
2021-04-10 15:58:04 +0300

    поменял api провайдера

--- 04 - MovieApp/js/app.js
+++ 04 - MovieApp/js/app.js
@@ -1,29 +1,20 @@
-const API_KEY = "f9974dfafac815fd53f9f7c31027313b";
-const API_URL_POPULAR = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ru-RU&page=1`;
-const API_URL_GENRES = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=ru-RU`;
-const API_SEARCH_QUERY = "";
+const API_KEY = "8c8e1a50-6322-4135-8875-5d40a5420d86";
+const API_URL_POPULAR =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
+const API_URL_SEARCH =
+  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 
 getMovies(API_URL_POPULAR);
 
 async function getMovies(url) {
-  const resp = await fetch(url);
+  const resp = await fetch(url, {
+    headers: {
+      "Content-Type": "aplication/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
   const respData = await resp.json();
-  showMovies(respData.results);
-}
-
-async function getMovieGenre(ids) {
-  const resp = await fetch(API_URL_GENRES);
-  const respData = await resp.json();
-
-  let result = [];
-  for (const [, value] of Object.entries(respData.genres)) {
-    ids.map((el) => {
-      if (el === value.id) {
-        result.push(value.name);
-      }
-    });
-  }
-  return result.join(", ");
+  showMovies(respData);
 }
 
 function getClassByRate(vote) {
@@ -36,32 +27,28 @@ function getClassByRate(vote) {
   }
 }
 
-function showMovies(movies) {
+function showMovies(data) {
   const moviesEl = document.querySelector(".movies");
 
   // Очищаем предыдущие фильмы
   document.querySelector(".movies").innerHTML = "";
 
-  movies.forEach(async (movie) => {
+  data.films.forEach(async (movie) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
         <div class="movie__cover-inner">
-          ${
-            movie.poster_path !== null
-              ? `<img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="movie__cover" alt="Годзилла против Конга">`
-              : `<img src="./img/no-image.png" class="movie__cover" alt="No image">`
-          }
+          ${`<img src="${movie.posterUrlPreview}" class="movie__cover" alt=${movie.nameRu}>`}
           <div class="movie__cover--darkened"></div>
         </div>
         <div class="movie__info">
-          <div class="movie__title">${movie.title}</div>
-          <div class="movie__category">${await getMovieGenre(
-            movie.genre_ids
+          <div class="movie__title">${movie.nameRu}</div>
+          <div class="movie__category">${movie.genres.map(
+            (genre) => ` ${genre.genre}`
           )}</div>
           <div class="movie__average movie__average--${getClassByRate(
-            movie.vote_average
-          )}">${movie.vote_average}</div> 
+            movie.rating
+          )}">${movie.rating}</div> 
         </div>
       `;
     moviesEl.appendChild(movieEl);
@@ -74,9 +61,8 @@ const search = document.querySelector(".header__search");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const searchTerm = search.value;
-  const apiSearchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=ru-RU&query=${searchTerm}&page=1&include_adult=false`;
-  if (searchTerm) {
+  const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
+  if (search.value) {
     getMovies(apiSearchUrl);
 
     search.value = "";
