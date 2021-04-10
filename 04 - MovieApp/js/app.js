b9cd99997ae97b2251072ccc3ef5c2b70ea3eefe
Adam Balkoev <adam22185515@gmail.com>
2021-04-10 21:25:56 +0300

    some fix

--- 04 - MovieApp/js/app.js
+++ 04 - MovieApp/js/app.js
@@ -9,7 +9,7 @@ getMovies(API_URL_POPULAR);
 async function getMovies(url) {
   const resp = await fetch(url, {
     headers: {
-      "Content-Type": "aplication/json",
+      "Content-Type": "application/json",
       "X-API-KEY": API_KEY,
     },
   });
@@ -33,24 +33,33 @@ function showMovies(data) {
   // Очищаем предыдущие фильмы
   document.querySelector(".movies").innerHTML = "";
 
-  data.films.forEach(async (movie) => {
+  data.films.forEach((movie) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
         <div class="movie__cover-inner">
-          ${`<img src="${movie.posterUrlPreview}" class="movie__cover" alt=${movie.nameRu}>`}
-          <div class="movie__cover--darkened"></div>
-        </div>
-        <div class="movie__info">
-          <div class="movie__title">${movie.nameRu}</div>
-          <div class="movie__category">${movie.genres.map(
-            (genre) => ` ${genre.genre}`
-          )}</div>
-          <div class="movie__average movie__average--${getClassByRate(
-            movie.rating
-          )}">${movie.rating}</div> 
-        </div>
-      `;
+        <img
+          src="${movie.posterUrlPreview}"
+          class="movie__cover"
+          alt="${movie.nameRu}"
+        />
+        <div class="movie__cover--darkened"></div>
+      </div>
+      <div class="movie__info">
+        <div class="movie__title">${movie.nameRu}</div>
+        <div class="movie__category">${movie.genres.map(
+          (genre) => ` ${genre.genre}`
+        )}</div>
+        ${
+          movie.rating &&
+          `
+        <div class="movie__average movie__average--${getClassByRate(
+          movie.rating
+        )}">${movie.rating}</div>
+        `
+        }
+      </div>
+        `;
     moviesEl.appendChild(movieEl);
   });
 }
