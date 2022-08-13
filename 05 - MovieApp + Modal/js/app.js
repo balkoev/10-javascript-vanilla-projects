0f7e46229818ac911f106c3cc76816abd3cbb42a
Adam Balkoev <adam22185515@gmail.com>
2022-08-13 17:30:39 +0300

    added project 05-MovieApp-Modal

--- 05 - MovieApp + Modal/js/app.js
+++ 05 - MovieApp + Modal/js/app.js
@@ -3,6 +3,7 @@ const API_URL_POPULAR =
   "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
 const API_URL_SEARCH =
   "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+const API_URL_MOVIE_DETAILS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/"
 
 getMovies(API_URL_POPULAR);
 
@@ -60,6 +61,7 @@ function showMovies(data) {
         }
       </div>
         `;
+    movieEl.addEventListener("click", () => openModal(movie.filmId))
     moviesEl.appendChild(movieEl);
   });
 }
@@ -77,3 +79,58 @@ form.addEventListener("submit", (e) => {
     search.value = "";
   }
 });
+
+
+// Modal
+const modalEl = document.querySelector(".modal");
+
+async function openModal(id) {
+  const resp = await fetch(API_URL_MOVIE_DETAILS + id, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
+  const respData = await resp.json();
+
+  modalEl.classList.add("modal--show")
+  document.body.classList.add("stop-scrolling");
+
+  modalEl.innerHTML = `
+    <div class="modal__card">
+      <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
+      <h2>
+        <span class="modal__movie-title">Название - ${respData.nameRu}</span>
+        <span class="modal__movie-release-year">Год - ${respData.year}</span>
+      </h2>
+      <ul class="modal__movie-info">
+        <div class="loader"></div>
+        <li class="modal__movie-genre">Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}</li>
+        ${respData.filmLength ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ''}
+        <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
+        <li class="modal__movie-overview">Описание - ${respData.description}</li>
+      </ul>
+      <button type="button" class="modal__button-close">Закрыть</button>
+    </div>
+  `
+  const btnClose = document.querySelector(".modal__button-close");
+  btnClose.addEventListener("click", () => closeModal())
+}
+
+async function closeModal() {
+  modalEl.classList.remove("modal--show")
+  document.body.classList.remove("stop-scrolling");
+}
+
+window.addEventListener("click", (e) => {
+  if (e.target === modalEl) {
+    closeModal();
+  }
+})
+
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode === 27) {
+    modalEl.classList.remove("modal--show")
+    document.body.classList.remove("stop-scrolling");
+  }
+})
