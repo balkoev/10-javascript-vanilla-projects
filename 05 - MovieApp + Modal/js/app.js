fa19ca46dfb00ee24b6f6a00f788a256a7c437a2
Adam Balkoev <adam22185515@gmail.com>
2022-08-13 16:10:52 +0300

    init

---
+++ 05 - MovieApp + Modal/js/app.js
@@ -0,0 +1,79 @@
+const API_KEY = "8c8e1a50-6322-4135-8875-5d40a5420d86";
+const API_URL_POPULAR =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
+const API_URL_SEARCH =
+  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+
+getMovies(API_URL_POPULAR);
+
+async function getMovies(url) {
+  const resp = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
+  const respData = await resp.json();
+  showMovies(respData);
+}
+
+function getClassByRate(vote) {
+  if (vote >= 7) {
+    return "green";
+  } else if (vote > 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+function showMovies(data) {
+  const moviesEl = document.querySelector(".movies");
+
+  // Очищаем предыдущие фильмы
+  document.querySelector(".movies").innerHTML = "";
+
+  data.films.forEach((movie) => {
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+        <div class="movie__cover-inner">
+        <img
+          src="${movie.posterUrlPreview}"
+          class="movie__cover"
+          alt="${movie.nameRu}"
+        />
+        <div class="movie__cover--darkened"></div>
+      </div>
+      <div class="movie__info">
+        <div class="movie__title">${movie.nameRu}</div>
+        <div class="movie__category">${movie.genres.map(
+          (genre) => ` ${genre.genre}`
+        )}</div>
+        ${
+          movie.rating &&
+          `
+        <div class="movie__average movie__average--${getClassByRate(
+          movie.rating
+        )}">${movie.rating}</div>
+        `
+        }
+      </div>
+        `;
+    moviesEl.appendChild(movieEl);
+  });
+}
+
+const form = document.querySelector("form");
+const search = document.querySelector(".header__search");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
+  if (search.value) {
+    getMovies(apiSearchUrl);
+
+    search.value = "";
+  }
+});
