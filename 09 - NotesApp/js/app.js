a32fe3415af1380f78ac76e77dafd756bd23fa9c
Adam Balkoev <adam22185515@gmail.com>
2022-09-11 19:44:35 +0300

    add project 09 - NotesApp

---
+++ 09 - NotesApp/js/app.js
@@ -0,0 +1,56 @@
+const notesEl = document.querySelector('.notes');
+const addBtn = document.querySelector('.note-add')
+
+function createNote(title, text) {
+  const noteEl = document.createElement('div');
+  noteEl.classList.add('note');
+  noteEl.innerHTML = `
+    <div class="note-header">
+      <p id="note-title">${title}</p>
+      <textarea id="note-title-input" class="hidden">${title}</textarea>
+      <div>
+        <button class="note-edit"><i class="fa-solid fa-pen-to-square"></i></button>
+        <button class="note-delete"><i class="fa-solid fa-trash"></i></button>
+      </div>
+    </div>
+    <p id="note-text">${text}</p>
+    <textarea id="note-textarea" class="hidden">${text}</textarea>
+
+  `
+
+  const editBtn = noteEl.querySelector('.note-edit');
+  const deleteBtn = noteEl.querySelector('.note-delete');
+  const titleEl = noteEl.querySelector('#note-title');
+  const textEl = noteEl.querySelector('#note-text');
+  const titleInputEl = noteEl.querySelector('#note-title-input');
+  const textInputEl = noteEl.querySelector('#note-textarea');
+
+
+
+  editBtn.addEventListener('click', (e) => {
+    titleEl.classList.toggle('hidden');
+    textEl.classList.toggle('hidden');
+
+    titleInputEl.classList.toggle('hidden');
+    textInputEl.classList.toggle('hidden');
+  });
+
+  deleteBtn.addEventListener('click', (e) => {
+    noteEl.remove();
+  });
+
+  titleInputEl.addEventListener('input', (e) => {
+    titleEl.innerText = e.target.value;
+  });
+
+  textInputEl.addEventListener('input', (e) => {
+    textEl.innerText = e.target.value;
+  });
+
+  return noteEl;
+}
+
+addBtn.addEventListener('click', (e) => {
+  const el = createNote("Заголовок", "Ваш текст");
+  notesEl.appendChild(el);
+});
